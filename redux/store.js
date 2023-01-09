66dae61b7729cfc302c4548bac5779298363ebef
Yaqub naqeb <yaqub.009448401@gmail.com>
2023-01-09 14:45:35 +0300

    adding reduxtoolkit done

---
+++ redux/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+import profileReducer from './reducers/profile'
+
+
+
+export default configureStore({
+    reducer:{
+        profile:profileReducer
+    }
+
+})
